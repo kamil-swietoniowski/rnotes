4b9e47971c078aa5e6251402ff2392c26a3a3592
Kamil Świętoniowski <kamilswietoniowski91@gmail.com>
2026-05-05 20:24:37 +0200

    feat: add remove feature and stdin

--- src/commands/note_command.rs
+++ src/commands/note_command.rs
@@ -29,9 +29,23 @@ pub fn parse_note(args: NoteArgs, conn: &Connection) {
     if args.get.is_some() {
         get_note(args.get, conn);
     }
+    if args.delete.is_some() {
+        remove_note(args.delete, conn);
+    }
 }
 
 pub fn add_note(title: Option<String>, content: Option<String>, conn: &Connection) {
+    let title = if title.is_none() {
+        Some(input("Enter title: "))
+    } else {
+        title
+    };
+
+    let content = if content.is_none() {
+        Some(input("Enter content: "))
+    } else {
+        content
+    };
     let note = Note::new(title, content);
     match note.add_to_database(conn) {
         Ok(_) => {}
@@ -56,4 +70,22 @@ pub fn list_notes(conn: &Connection) {
 pub fn get_note(id: Option<i32>, conn: &Connection) {
     let note = Note::get_from_database(id.unwrap(), conn).unwrap();
     note.display(true);
+}
+
+pub fn remove_note(id: Option<i32>, conn: &Connection) {
+    Note::remove_from_database(id.unwrap(), conn).unwrap();
+}
+
+
+use std::io::{self, Write};
+fn input(querry: &str) -> String {
+    let mut buffer = String::new();
+
+    print!("{}", querry);
+
+    io::stdout().flush().expect("Stdout error");
+
+    io::stdin().read_line(&mut buffer).expect("Cant read the line");
+
+    buffer.trim().to_string()
 }
